40315ecf15c8dc727491a88a8b0243605fbe4515
David Shahin Morady <david_shahin.morady@smail.th-koeln.de>
2018-09-11 17:22:06 +0200

    Pair Programming Solution from Gorock and Shahin852

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -1,4 +1,6 @@
 """Solve the spy game!"""
+import numpy as np
+
 
 class SafetyFinder:
     """A class that contains everything we need to find the
@@ -15,7 +17,9 @@ class SafetyFinder:
 
         Returns a list of coordinates in zero-indexed vector form.
         """
-        pass
+
+        return [[ord(i[0]) - ord('A'), int(i[1:]) - 1]
+                for i in agents]
 
     def find_safe_spaces(self, agents):
         """This method will take an array with agent locations and find
@@ -28,7 +32,19 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
-        pass
+
+        distance_matrix = np.full((10, 10), 100, dtype=int)
+
+        for agent_position in agents:
+            for x in range(10):
+                for y in range(10):
+                    distance = abs(x - agent_position[0]) + abs(y - agent_position[1])
+                    if distance < distance_matrix[x][y]:
+                        distance_matrix[x][y] = distance
+
+        list_of_tuples = np.where(distance_matrix == distance_matrix.max())
+
+        return [[x, y] for x, y in zip(list_of_tuples[0], list_of_tuples[1])]
 
     def advice_for_alex(self, agents):
         """This method will take an array with agent locations and offer advice
@@ -42,4 +58,15 @@ class SafetyFinder:
         Returns either a list of alphanumeric map coordinates for Alex to hide in,
         or a specialized message informing her of edge cases
         """
-        pass
+        if len(agents) == 100:
+            return 'There are no safe locations for Alex! :-('
+        if len(agents) == 0:
+            return 'The whole city is safe for Alex! :-)'
+
+        converted_coordinates = self.convert_coordinates(agents)
+        for i in converted_coordinates:
+            if i[0] < 0 or i[1] > 9 or i[0] > 9 or i[1] < 0:
+                return 'The whole city is safe for Alex! :-)'
+        result = self.find_safe_spaces(converted_coordinates)
+
+        return [f'{chr(i[0]+ord("A"))}{i[1]+1}' for i in result]

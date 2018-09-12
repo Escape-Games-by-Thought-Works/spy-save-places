f6191790c7cc110220e243bc56923ed2d3e59c3a
Roland Schmitz <gitours@2rs.de>
2018-09-12 08:13:55 +0200

    implemented python solution

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -15,8 +15,34 @@ class SafetyFinder:
 
         Returns a list of coordinates in zero-indexed vector form.
         """
-        pass
+        convertedAgents = [ ]
+        for agent in agents:
+            x = ord(agent[0])-ord('A')
+            if x<0 : raise IndexError('Bad X value {} in {} from {}'.format(x, agent, agents))
+            y=int(agent[1:])-1
+            if y<0 : raise IndexError('Bad Y value {} in {} from {}'.format(y, agent, agents))
+            convertedAgents.append([x,y])
+        return convertedAgents
+    
+    def convert_coordinates_back(self, agents):
+        """This method should take a list of coordinates arrays (e.g. [0, 5]) with zero-indexing
+        and return an array of alphanumeric coordinates (e.g. 'A6').
+        For instance, [[0,5]] should become ['A6'].
 
+        Arguments:
+        agents -- a list of coordinates in zero-indexed vector form.
+
+        Returns a list-like object containing alphanumeric coordinates.
+        """
+        convertedAgents = [ ]
+        for agentXY in agents:
+            x,y = agentXY
+            convertedX = "ABCDEFGHIJ"[x]
+            convertedY = y+1
+            convertedXY = "{}{}".format(convertedX, convertedY)
+            convertedAgents.append(convertedXY)
+        return convertedAgents
+    
     def find_safe_spaces(self, agents):
         """This method will take an array with agent locations and find
         the safest places in the city for Alex to hang out.
@@ -28,7 +54,21 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
-        pass
+        safePlaces = []
+        maxMinDistance = 1
+        for x in range(0,10) :
+            for y in range(0,10):
+                minDistance = 9999
+                for agent in agents:
+                    [ax,ay] = agent
+                    if ax<10 and ay<10 :
+                        distance = abs(ax - x) + abs(ay - y)
+                        if distance < minDistance: minDistance = distance
+                if minDistance > maxMinDistance :
+                    maxMinDistance = minDistance
+                    safePlaces = []
+                if minDistance == maxMinDistance : safePlaces.append([x,y])
+        return safePlaces
 
     def advice_for_alex(self, agents):
         """This method will take an array with agent locations and offer advice
@@ -42,4 +82,9 @@ class SafetyFinder:
         Returns either a list of alphanumeric map coordinates for Alex to hide in,
         or a specialized message informing her of edge cases
         """
-        pass
+        agentCoordinates = self.convert_coordinates(agents)
+        safePlaces = self.find_safe_spaces(agentCoordinates)
+        safePlacesCount = len(safePlaces)
+        if safePlacesCount == 0 : return 'There are no safe locations for Alex! :-('
+        if safePlacesCount == 100 : return 'The whole city is safe for Alex! :-)'
+        return self.convert_coordinates_back(safePlaces)

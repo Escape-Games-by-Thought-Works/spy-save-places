719c77547bec33600fea4bb3dd1c2bdd70b88443
Silvia Santano <ssantano@inovex.de>
2018-09-18 12:13:28 +0200

    silvias-python-solution

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -1,4 +1,6 @@
 """Solve the spy game!"""
+import itertools
+import numpy
 
 class SafetyFinder:
     """A class that contains everything we need to find the
@@ -15,7 +17,29 @@ class SafetyFinder:
 
         Returns a list of coordinates in zero-indexed vector form.
         """
-        pass
+
+        coordinates = []
+        for agent in agents:
+
+            if ord(agent[0]) < 97:
+                convert_ascii = 65
+            else:
+                convert_ascii = 97
+
+            x = ord(agent[0])-convert_ascii
+            y = int(agent[1:])-1
+            if x<= 10 and y <= 10:
+                coordinates.append([x, y])
+        return coordinates
+
+    def unconvert_coordinates(self, agents):
+
+        coordinates = []
+        for agent in agents:
+            x = str(""+chr(agent[0]+65))
+            y = str(agent[1]+1)
+            coordinates.append(x + y)
+        return coordinates
 
     def find_safe_spaces(self, agents):
         """This method will take an array with agent locations and find
@@ -28,7 +52,21 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
-        pass
+        distances = [[0 for x in range(10)] for y in range(10)]
+
+        for (x,y) in itertools.product(range(10), range(10)):
+
+            distances_to_agents = []
+            for agent in agents:
+                distances_to_agents.append(self.distance([x,y], agent))
+            dist = min(distances_to_agents)
+            distances[x][y] = dist
+        longest_distance = max(map(max,distances))
+        return([[ix,iy] for ix, row in enumerate(distances) for iy, i in enumerate(row) if i == longest_distance])
+
+    def distance(self, p0, p1):
+        """ Compute distance between two coordinates """
+        return abs(p1[0] - p0[0]) + abs(p1[1] - p0[1])
 
     def advice_for_alex(self, agents):
         """This method will take an array with agent locations and offer advice
@@ -42,4 +80,11 @@ class SafetyFinder:
         Returns either a list of alphanumeric map coordinates for Alex to hide in,
         or a specialized message informing her of edge cases
         """
-        pass
+        agents = self.convert_coordinates(agents)
+
+        if not agents:
+            return 'The whole city is safe for Alex! :-)'
+        if len(agents) == 100:
+            return 'There are no safe locations for Alex! :-('
+
+        return self.unconvert_coordinates(self.find_safe_spaces(agents))

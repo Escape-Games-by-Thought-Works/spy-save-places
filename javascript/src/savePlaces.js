fd5e54dace5cb57884ad5ebe91494cd22b0bc49a
Bettina Weinholtz <bettina.weinholtz@tum.de>
2019-01-28 00:53:15 +0100

    Try with just looking in the same line/row for agents

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -1,4 +1,5 @@
-// This is where you implement your solution 
+// This is where you implement your solution
+
 const convertCoordinates = (agents) => {
   const numberOfAgents = agents.length;
   if (numberOfAgents === 0) {
@@ -25,14 +26,178 @@ const convertCoordinates = (agents) => {
   }
 }
 
+const citySizeX = 10;
+const citySizeY = 10;
+
 const findSafePlaces = (agents) => {
-  var cityArray = Array(10, 10);
-  return "findSafePlaces"
+  var cityArray = new Array(citySizeX).fill(null).map(()=>new Array(citySizeY).fill(null));
+  console.log(cityArray);
+  var x, y, i;
+
+  // Add agent locations
+  for (x = 0; x < citySizeX; x++) {
+    for (y = 0; y < citySizeY; y++) {
+      for (i = 0; i < agents.length; i++) {
+        if (x === agents[i][0] && y === agents[i][1]) {
+          cityArray[x][y] = 0;
+        }
+      }
+    }
+  }
+  // console.log(cityArray);
+
+  var maximumDistance = 0;
+
+  // Walking from left to right
+  const leftDistances = calculateLeftDistances(cityArray, agents);
+  // console.log(leftDistances);
+
+  // Walking from right to left
+  const rightDistances = calculateRightDistances(cityArray, agents);
+  // console.log(rightDistances);
+
+  // Walking from top to bottom
+  const topDistances = calculateTopDistances(cityArray, agents);
+  // console.log(topDistances);
+
+  // Walking from bottom to top
+  const bottomDistances = calculateBottomDistances(cityArray, agents);
+  // console.log(bottomDistances);
+
+  // Calculate minimum distances to agents
+  const minDistances = calculateMinDistances(leftDistances, rightDistances, topDistances, bottomDistances);
+  // console.log(minDistances);
+
+  // Find maximum distance from agent
+  const safeDistance = Math.max(...minDistances);
+  // console.log(safeDistance);
+
+  var safePlaces = [];
+
+  for (var x = 0; x < citySizeX; x++) {
+    for (var y = 0; y < citySizeY; y++) {
+      if (cityArray[x][y] === safeDistance) {
+        if (safePlaces === []) {
+          safePlaces = [[x, y]];
+        } else {
+          safePlaces.push([x, y]);
+        }
+      }
+    }
+  }
+  console.log(safePlaces);
+  return safePlaces;
+}
+
+
+function calculateLeftDistances(cityArray, agents) {
+  var agentDistanceLeft;
+  for (x = 0; x < citySizeX; x++) {
+    agentDistanceLeft = 0;
+    var agentDiscoveredInLine = false;
+    for (y = 0; y < citySizeY; y++) {
+      if (agentDiscoveredInLine) {
+        agentDistanceLeft++;
+      }
+
+      for (i = 0; i < agents.length; i++) {
+
+        if (x === agents[i][0] && y === agents[i][1]) {
+          agentDiscoveredInLine = true;
+          agentDistanceLeft = 0;
+        }
+
+      }
+      cityArray[x][y] = agentDistanceLeft;
+    }
+  }
+  return cityArray;
+}
+
+function calculateRightDistances(cityArray, agents) {
+  var agentDistanceRight = 0;
+  for (var x = 0; x < citySizeX; x++) {
+    agentDistanceRight = 0;
+    var agentDiscoveredInLine = false;
+    for (var y = citySizeY - 1; y >= 0; y--) {
+      if (agentDiscoveredInLine) {
+        agentDistanceRight++;
+      }
+      for (var i = 0; i < agents.length; i++) {
+
+        if (x === agents[i][0] && y === agents[i][1]) {
+          agentDiscoveredInLine = true;
+          agentDistanceRight = 0;
+        }
+
+      }
+      cityArray[x][y] = agentDistanceRight;
+    }
+  }
+  return cityArray;
+}
+
+function calculateTopDistances(cityArray, agents) {
+  var agentDistanceTop = 0;
+  for (var y = 0; y < citySizeY; y++) {
+    agentDistanceTop = 0;
+    var agentDiscoveredInColumn = false;
+    for (var x = 0; x < citySizeX; x++) {
+      if (agentDiscoveredInColumn) {
+        agentDistanceTop++;
+      }
+      for (var i = 0; i < agents.length; i++) {
+
+        if (x === agents[i][0] && y === agents[i][1]) {
+          agentDiscoveredInColumn = true;
+          agentDistanceTop = 0;
+        }
+
+      }
+      cityArray[x][y] = agentDistanceTop;
+    }
+  }
+  return cityArray;
+}
+
+function calculateBottomDistances(cityArray, agents) {
+  var agentDistanceBottom = 0;
+  for (var y = 0; y < citySizeY; y++) {
+    agentDistanceBottom = 0;
+    var agentDiscoveredInColumn = false;
+    for (var x = citySizeX - 1; x >= 0; x--) {
+      if (agentDiscoveredInColumn) {
+        agentDistanceBottom++;
+      }
+      for (var i = 0; i < agents.length; i++) {
+
+        if (x === agents[i][0] && y === agents[i][1]) {
+          agentDiscoveredInColumn = true;
+          agentDistanceBottom = 0;
+        }
+
+      }
+      cityArray[x][y] = agentDistanceBottom;
+    }
+  }
+  return cityArray;
+}
+
+function calculateMinDistances(leftDistances, rightDistances, topDistances, bottomDistances) {
+  var cityArray = new Array(citySizeX).fill(null).map(()=>new Array(citySizeY).fill(null));
+  for (var x = 0; x < citySizeX; x++) {
+    for (var y = 0; y < citySizeY; y++) {
+      cityArray[x][y] = Math.min(leftDistances[x][y], rightDistances[x][y], topDistances[x][y], bottomDistances[x][y]);
+    }
+  }
+  return cityArray;
 }
 
 const adviceForAlex = (agents) => {
   return "adviceForAlex"
 }
+
+
 
 module.exports = {
   convertCoordinates,
--- javascript/test/savePlaces.test.js
+++ javascript/test/savePlaces.test.js
@@ -2,7 +2,7 @@ const { convertCoordinates, findSafePlaces, adviceForAlex } = require('../src/sa
 
 // Please enable Level 1, 2, 3-Tests by replacing xdescribe with describe!
 // Do not edit the tests itself!
-describe('Spy Places Level 1 - convert coordinates', () => {
+xdescribe('Spy Places Level 1 - convert coordinates', () => {
   it('no agents return empty array', () => {
     expect(convertCoordinates([])).toEqual([])
   })
@@ -23,7 +23,7 @@ describe('Spy Places Level 1 - convert coordinates', () => {
   })
 })
 
-xdescribe('Spy Places Level 2 - find save places', () => {
+describe('Spy Places Level 2 - find save places', () => {
   it('some places are save if agents are some', () => {
     const agents = [[1, 1], [3, 5], [4, 8], [7, 3], [7, 8], [9, 1]]
     expect(findSafePlaces(agents).length).toEqual(3)

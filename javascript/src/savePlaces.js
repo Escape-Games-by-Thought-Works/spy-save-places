62bfc8a60117f7b378f055c8af620cc09715f219
Bettina Weinholtz <bettina.weinholtz@tum.de>
2019-01-28 12:25:22 +0100

    Add solution for test level 2

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -30,52 +30,43 @@ const citySizeX = 10;
 const citySizeY = 10;
 
 const findSafePlaces = (agents) => {
-  var cityArray = new Array(citySizeX).fill(null).map(()=>new Array(citySizeY).fill(null));
+  const distanceUpperLimit = citySizeX + citySizeY;
+  var cityArray = new Array(citySizeX).fill(null).map(()=>new Array(citySizeY).fill(distanceUpperLimit));
   console.log(cityArray);
+
   var x, y, i;
 
-  // Add agent locations
+  // Iterate over fields and calculate their minimum distance to each agent
   for (x = 0; x < citySizeX; x++) {
     for (y = 0; y < citySizeY; y++) {
       for (i = 0; i < agents.length; i++) {
-        if (x === agents[i][0] && y === agents[i][1]) {
-          cityArray[x][y] = 0;
+        const distanceToAgentX = Math.abs(agents[i][0] - x);
+        const distanceToAgentY = Math.abs(agents[i][1] - y);
+        const distanceToAgent = distanceToAgentX + distanceToAgentY;
+        const minDistanceToAgents = cityArray[x][y];
+        if (distanceToAgent < minDistanceToAgents) {
+          cityArray[x][y] = distanceToAgent;
         }
       }
     }
   }
-  // console.log(cityArray);
+  console.log(cityArray);
 
-  var maximumDistance = 0;
+  // Find maximum distance value (safe distance)
+  var safeDistance = 0;
+  for (x = 0; x < citySizeX; x++) {
+    for (y = 0; y < citySizeY; y++) {
+      if (cityArray[x][y] > safeDistance) {
+        safeDistance = cityArray[x][y];
+      }
+    }
+  }
 
-  // Walking from left to right
-  const leftDistances = calculateLeftDistances(cityArray, agents);
-  // console.log(leftDistances);
-
-  // Walking from right to left
-  const rightDistances = calculateRightDistances(cityArray, agents);
-  // console.log(rightDistances);
-
-  // Walking from top to bottom
-  const topDistances = calculateTopDistances(cityArray, agents);
-  // console.log(topDistances);
-
-  // Walking from bottom to top
-  const bottomDistances = calculateBottomDistances(cityArray, agents);
-  // console.log(bottomDistances);
-
-  // Calculate minimum distances to agents
-  const minDistances = calculateMinDistances(leftDistances, rightDistances, topDistances, bottomDistances);
-  // console.log(minDistances);
-
-  // Find maximum distance from agent
-  const safeDistance = Math.max(...minDistances);
-  // console.log(safeDistance);
-
+  // Identify safe places (indices with values equal to safe distance)
   var safePlaces = [];
 
-  for (var x = 0; x < citySizeX; x++) {
-    for (var y = 0; y < citySizeY; y++) {
+  for (x = 0; x < citySizeX; x++) {
+    for (y = 0; y < citySizeY; y++) {
       if (cityArray[x][y] === safeDistance) {
         if (safePlaces === []) {
           safePlaces = [[x, y]];
@@ -90,113 +81,9 @@ const findSafePlaces = (agents) => {
 }
 
 
-function calculateLeftDistances(cityArray, agents) {
-  var agentDistanceLeft;
-  for (x = 0; x < citySizeX; x++) {
-    agentDistanceLeft = 0;
-    var agentDiscoveredInLine = false;
-    for (y = 0; y < citySizeY; y++) {
-      if (agentDiscoveredInLine) {
-        agentDistanceLeft++;
-      }
-
-      for (i = 0; i < agents.length; i++) {
-
-        if (x === agents[i][0] && y === agents[i][1]) {
-          agentDiscoveredInLine = true;
-          agentDistanceLeft = 0;
-        }
-
-      }
-      cityArray[x][y] = agentDistanceLeft;
-    }
-  }
-  return cityArray;
-}
-
-function calculateRightDistances(cityArray, agents) {
-  var agentDistanceRight = 0;
-  for (var x = 0; x < citySizeX; x++) {
-    agentDistanceRight = 0;
-    var agentDiscoveredInLine = false;
-    for (var y = citySizeY - 1; y >= 0; y--) {
-      if (agentDiscoveredInLine) {
-        agentDistanceRight++;
-      }
-      for (var i = 0; i < agents.length; i++) {
-
-        if (x === agents[i][0] && y === agents[i][1]) {
-          agentDiscoveredInLine = true;
-          agentDistanceRight = 0;
-        }
-
-      }
-      cityArray[x][y] = agentDistanceRight;
-    }
-  }
-  return cityArray;
-}
-
-function calculateTopDistances(cityArray, agents) {
-  var agentDistanceTop = 0;
-  for (var y = 0; y < citySizeY; y++) {
-    agentDistanceTop = 0;
-    var agentDiscoveredInColumn = false;
-    for (var x = 0; x < citySizeX; x++) {
-      if (agentDiscoveredInColumn) {
-        agentDistanceTop++;
-      }
-      for (var i = 0; i < agents.length; i++) {
-
-        if (x === agents[i][0] && y === agents[i][1]) {
-          agentDiscoveredInColumn = true;
-          agentDistanceTop = 0;
-        }
-
-      }
-      cityArray[x][y] = agentDistanceTop;
-    }
-  }
-  return cityArray;
-}
-
-function calculateBottomDistances(cityArray, agents) {
-  var agentDistanceBottom = 0;
-  for (var y = 0; y < citySizeY; y++) {
-    agentDistanceBottom = 0;
-    var agentDiscoveredInColumn = false;
-    for (var x = citySizeX - 1; x >= 0; x--) {
-      if (agentDiscoveredInColumn) {
-        agentDistanceBottom++;
-      }
-      for (var i = 0; i < agents.length; i++) {
-
-        if (x === agents[i][0] && y === agents[i][1]) {
-          agentDiscoveredInColumn = true;
-          agentDistanceBottom = 0;
-        }
-
-      }
-      cityArray[x][y] = agentDistanceBottom;
-    }
-  }
-  return cityArray;
-}
-
-function calculateMinDistances(leftDistances, rightDistances, topDistances, bottomDistances) {
-  var cityArray = new Array(citySizeX).fill(null).map(()=>new Array(citySizeY).fill(null));
-  for (var x = 0; x < citySizeX; x++) {
-    for (var y = 0; y < citySizeY; y++) {
-      cityArray[x][y] = Math.min(leftDistances[x][y], rightDistances[x][y], topDistances[x][y], bottomDistances[x][y]);
-    }
-  }
-  return cityArray;
-}
-
 const adviceForAlex = (agents) => {
   return "adviceForAlex"
 }
-
 
 
 module.exports = {

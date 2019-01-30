80e34c2c4b2f1516243f9e7a812a1fa236f4086c
Fabian Sturm <f@rtfs.org>
2019-01-30 22:13:08 +0100

    All tests pass except one cornercase

--- python/safe_spaces.py
+++ python/safe_spaces.py
@@ -1,4 +1,5 @@
 """Solve the spy game!"""
+import numpy
 
 class SafetyFinder:
     """A class that contains everything we need to find the
@@ -20,6 +21,12 @@ class SafetyFinder:
             positions.append([ord(coordinate[0])-ord("A"), int(coordinate[1:])-1])
         return positions
 
+    def convert_coordinates_reverse(self, agents):
+        positions = []
+        for coordinate in agents:
+            positions.append(chr(coordinate[0]+ord("A")) + str(coordinate[1]+1))
+        return positions
+
     def find_safe_spaces(self, agents):
         """This method will take an array with agent locations and find
         the safest places in the city for Alex to hang out.
@@ -31,7 +38,40 @@ class SafetyFinder:
 
         Returns a list of safe spaces in indexed vector form.
         """
-        pass
+        _, safe_spaces = self.find_safe_spaces_with_distance(agents)
+        return safe_spaces
+
+    def find_safe_spaces_with_distance(self, agents):
+        city_length = 10
+        maxvalue = 2 * city_length - 1;  # From one corner of the city to the other
+        map = numpy.full((city_length, city_length), maxvalue)
+
+        # Now fill the map with distances for each agent if it is smaller then already set
+        for agent in agents:
+            for row in range(10):
+                for column in range(10):
+                    distance_row = row - agent[0]
+                    distance_column = column - agent[1]
+                    distance = abs(distance_row) + abs(distance_column)
+                    map[column, row] = min(map[column, row], distance)
+
+        # Now find the longest distance
+        longest_distance = 1
+        for row in range(10):
+            for column in range(10):
+                longest_distance = max(longest_distance, map[column, row])
+
+        # Filter coordinates with longest distance
+        coordinates = []
+        for row in range(10):
+            for column in range(10):
+                if map[column, row] == longest_distance:
+                    coordinates.append([row, column])
+        print(map)
+        print(longest_distance)
+        print(coordinates)
+
+        return longest_distance, coordinates
 
     def advice_for_alex(self, agents):
         """This method will take an array with agent locations and offer advice
@@ -45,4 +85,16 @@ class SafetyFinder:
         Returns either a list of alphanumeric map coordinates for Alex to hide in,
         or a specialized message informing her of edge cases
         """
-        pass
+        agents_indexed = self.convert_coordinates(agents)
+        distance, safe_spaces_indexed = self.find_safe_spaces_with_distance(agents_indexed)
+
+        if distance == 19:
+            return "The whole city is safe for Alex! :-)"
+        if distance == 0:
+            return "There are no safe locations for Alex! :-("
+
+
+        safe_spaces = self.convert_coordinates_reverse(safe_spaces_indexed)
+        print(safe_spaces)
+        return safe_spaces
+

a802bbab187491785e4c9b2ed72330839cfab893
tweini <tweini@web.de>
2019-04-19 00:58:01 +0200

    implemented convertCoordinates()

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -1,6 +1,20 @@
 // This is where you implement your solution 
-const convertCoordinates = (agents) => {
-  return [[0,0],[1,2]]
+const convertCoordinates = (agents  = []) => {
+  
+  Array.isArray(agents) || (agents = [agents])
+
+  return agents.map((coord, i) => {
+
+    /** @type String */
+    const xCoord_unparsed = coord[0]
+    /** @type String */
+    const yCoord_unparsed = (coord.length == 2) ? coord[1] : '' + coord[1] + coord[2]
+    
+    let xCoord = xCoord_unparsed.charCodeAt(0) - 65
+    let yCoord = parseInt(yCoord_unparsed) - 1
+
+    return [xCoord,yCoord]
+  })
 }
 
 const findSafePlaces = (agents) => {

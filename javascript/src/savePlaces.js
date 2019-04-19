980149aa8060726efaa047789ecd2b8379696bac
tweini <tweini@web.de>
2019-04-19 07:42:21 +0200

    implemented findSafePlaces(), adviceForAlex()

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -18,11 +18,95 @@ const convertCoordinates = (agents  = []) => {
 }
 
 const findSafePlaces = (agents) => {
-  return "findSafePlaces"
+  
+  /* create a map aka Distance Array (distArray) to store the Distance-to-Agent (distToAgent) Values */
+  let distArray = Array(10).fill(0)
+  for(let i = 0; i<10; i++) distArray[i] = Array(10).fill(0)
+
+  let distToAgent = 1;
+  let spotsToVisit = []
+  let nextSpotsToVisit = []
+  let safePlaces = []
+
+  const isNotOutOfBounds = ([xCoord, yCoord]) => ( ! ((xCoord < 0) || (xCoord > 9) || (yCoord < 0) || (yCoord > 9)) )
+  
+  agents = agents.filter(isNotOutOfBounds)
+  for (agent of agents){
+    let [xCoord, yCoord] = agent
+    distArray[xCoord][yCoord] = 'X'
+  }
+
+  /* No Agents must return an Array with ervery spot as a safe spot */
+  if (agents.length == 0){
+    for (let i=0; i<10; i++){
+      for (let j=0; j<10; j++){
+        safePlaces.push([i,j])
+      }
+    }
+
+    return safePlaces
+  }
+
+  /* 100 Agents must return an empty Array with no safe spot */
+  if (agents.length == 100){
+
+    return []
+  }
+
+  spotsToVisit = agents
+
+  do{
+    for (spot of spotsToVisit){
+      let [xCoord, yCoord] = spot
+
+      for (direction of [[1,0],[0,1],[-1,0],[0,-1]]){
+        if (isNotOutOfBounds([xCoord + direction[0], yCoord + direction[1]])){
+          if (distArray[xCoord + direction[0]][yCoord + direction[1]] == 0){
+            distArray[xCoord + direction[0]][yCoord + direction[1]] = distToAgent
+
+            nextSpotsToVisit.push([xCoord + direction[0], yCoord + direction[1]])
+          } 
+        }
+      }
+    }
+
+    safePlaces = spotsToVisit
+    spotsToVisit = nextSpotsToVisit
+    nextSpotsToVisit = []
+    distToAgent++
+
+  } while (spotsToVisit.length)
+
+  return safePlaces
 }
 
 const adviceForAlex = (agents) => {
-  return "adviceForAlex"
+
+  const reConvertCoordinates = (agents  = []) => {
+  
+    Array.isArray(agents) || (agents = [agents])
+  
+    return agents.map((coord, i) => {
+  
+      const xCoord = coord[0]
+      const yCoord = coord[1]
+      
+      let xCoord_alphanum = String.fromCharCode(xCoord + 65)
+      let yCoord_alphanum = (yCoord+1).toString()
+  
+      return xCoord_alphanum + yCoord_alphanum
+    })
+  }
+
+  agents = convertCoordinates(agents)
+  let safePlaces = findSafePlaces(agents)
+  safePlaces = reConvertCoordinates(safePlaces)
+
+  return safePlaces.length == 100 ?
+         'The whole city is safe for Alex! :-)' :
+         safePlaces.length == 0 ?
+         'There are no safe locations for Alex! :-(' :
+         safePlaces
 }
 
 module.exports = {

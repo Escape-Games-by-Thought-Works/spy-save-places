1f862ca3eb1035e3c339628a6be4eb8d2d7837c1
Lidiia Lazebnikova <lidiia.lazebnikova@moebel.de>
2019-05-16 20:05:50 +0200

    First scenario.

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -1,18 +1,33 @@
-// This is where you implement your solution 
-const convertCoordinates = (agents) => {
-  return [[0,0],[1,2]]
-}
+// This is where you implement your solution
+const convertCoordinates = agents => {
+  const xCoordinates = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const yCoordinates = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  if (!agents) {
+    throw new Error("Not a valid agent!");
+  }
+  if (agents.length === 0) {
+    return [];
+  } else {
+    return agents.map(agent => {
+      const secondCoordinate = agent.substr(1, agent.length - 1);
+      return [
+        xCoordinates.indexOf(agent[0]),
+        yCoordinates.indexOf(secondCoordinate)
+      ];
+    });
+  }
+};
 
-const findSafePlaces = (agents) => {
-  return "findSafePlaces"
-}
+const findSafePlaces = agents => {
+  return "findSafePlaces";
+};
 
-const adviceForAlex = (agents) => {
-  return "adviceForAlex"
-}
+const adviceForAlex = agents => {
+  return "adviceForAlex";
+};
 
 module.exports = {
   convertCoordinates,
   findSafePlaces,
   adviceForAlex
-}
+};

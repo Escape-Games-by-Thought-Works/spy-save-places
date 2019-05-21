2f60a20d4e317237c695ab23bdfd1fc43f4cf47d
Thomas Holst <t.holst@sap.com>
2019-05-21 19:10:46 +0200

    implemented Spy Places Level 2 - find safe places

--- javascript/src/savePlaces.js
+++ javascript/src/savePlaces.js
@@ -11,8 +11,32 @@ const convertCoordinates = (agents) => {
   return aCoords;
 }
 
+const findClosestAgentDistance = (x, y, aAgentCoords) => {
+  let aAgentDistances = aAgentCoords.map(aAgentCoord => Math.abs(x - aAgentCoord[0]) + Math.abs(y - aAgentCoord[1])); 
+  console.log(aAgentDistances);
+  console.log(Math.min(...aAgentDistances));
+  return Math.min(...aAgentDistances);
+}
+
 const findSafePlaces = (agents) => {
-  return "findSafePlaces"
+  let aSafePlaces = [];
+  let iSafeDistance = 1;
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let iClosestAgentDistance = findClosestAgentDistance(i, j, agents);
+      if (iClosestAgentDistance > iSafeDistance) {
+        // increase what distance we consider a SAFE distance
+        iSafeDistance = iClosestAgentDistance;
+        aSafePlaces = [[i, j]];
+      } else if (iClosestAgentDistance === iSafeDistance) {
+        // add location to safe places
+        aSafePlaces.push([i, j]);
+      }
+    }
+  }
+
+  return aSafePlaces;
 }
 
 const adviceForAlex = (agents) => {
